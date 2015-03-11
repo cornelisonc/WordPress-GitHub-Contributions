83e5de561f920861e5a1a62bba73827ec5012112
Ceili Cornelison <ceili@ceilicornelison.com>
2015-03-11 12:23:50 -0400

    Basic plugin file

---
+++ wordpress-github-contributions.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Wordpress GitHub Contributions
+Plugin URI: http://www.ceilicornelison.com
+Description: Adds a visual display of GitHub actions by day
+Version: 0.1
+Author: Ceili Cornelison
+Author URI: http://www.ceilicornelison.com
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: wg-contrib
+*/
